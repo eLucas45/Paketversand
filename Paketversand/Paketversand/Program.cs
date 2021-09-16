a0a3cb927ea22f82eead4c6be9acc76aba91842c
eLucas45 <contact.limiteddev@gmail.com>
2021-09-16 11:00:49 +0200

    finished shit still does not work fml

--- Paketversand/Paketversand/Program.cs
+++ Paketversand/Paketversand/Program.cs
@@ -15,15 +15,21 @@ namespace Paketversand
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            //Code here
+            int MaterialStrength;
+            double Density;
             Console.Title = "Paketversandrechner";
             Explanation();
 
             //string[] PackageDim = new string[2];
-            string[] PackageDim = RequestData();
-            //WorkWithData();
+            //PackageDim = Width + Length
+            int[] PackageDim = RequestData();
+            WorkWithData();
             NoClose();
         }
+
+        public static 
+
+
         /// <summary>
         /// Displays the Explanation of what the Program does
         /// </summary>
@@ -36,20 +42,80 @@ namespace Paketversand
         /// <summary>
         /// Request Data from User
         /// </summary>
-        public static string[] RequestData()
+        public static int[] RequestData()
         {
             //string[] Dim = new string[2];
+            int inputWidth = ReqWidth();
+            int inputLength = ReqLength();
             
-            Console.Write("Bitte gebe die Breite des Pakets in cm ein: ");
-            string inputWidth = Console.ReadLine();
-            Console.Write("Bitte gebe die Länge des Pakets in cm ein: ");
-            string inputLength = Console.ReadLine();
             //PackageDim
             //Dim[0] = inputWidth;
             //Dim[1] = inputLength;
-            string[] Dim = { inputWidth, inputLength };
+            int[] Dim = { inputWidth, inputLength };
             Console.Write(Dim[0] + " x " + Dim[1]);
             return Dim;
+        }
+        public static int ReqLength()
+        {
+            int Length = 0;
+            Console.Write("Bitte gebe die Länge des Pakets in cm ein: ");
+            string inputLength = Console.ReadLine();
+            if (CheckIfConvertable(inputLength))
+            {
+                Length = int.Parse(inputLength);
+                return Length;
+            }
+            else
+            {
+                Length = ReqLength2();
+                return Length;
+            }
+        }
+
+        public static int ReqLength2()
+        {
+            int Lenth = 250;
+            string inputLength = "this is text";
+            while (!CheckIfConvertable(inputLength))
+            {
+                Console.Write("Bitte gebe die Länge des Pakets in cm ein: ");
+                inputLength = Console.ReadLine();
+            }
+            return Lenth;
+        }
+
+        public static int ReqWidth2()
+        {
+            int Width = 250;
+            string inputWidth = "this is text";
+            while (!CheckIfConvertable(inputWidth))
+            {
+                Console.Write("Bitte gebe die Breite des Pakets in cm ein: ");
+                inputWidth = Console.ReadLine();
+            }
+            return Width;
+        }
+
+
+        /// <summary>
+        /// Gets the Width from the user
+        /// </summary>
+        /// <returns></returns>
+        public static int ReqWidth()
+        {
+            int Width = 0;
+            Console.Write("Bitte gebe die Breite des Pakets in cm ein: ");
+            string inputWidth = Console.ReadLine();
+            if (CheckIfConvertable(inputWidth))
+            {
+                Width = int.Parse(inputWidth);
+                return Width;
+            }
+            else
+            {
+                Width = ReqWidth2();
+                return Width;
+            }
         }
 
         /// <summary>
@@ -57,7 +123,7 @@ namespace Paketversand
         /// </summary>
         /// <param name="input"></param>
         /// <returns></returns>
-        public static bool CheckIfConvertable(String input)
+        public static bool CheckIfConvertable(string input)
         {
             bool isConvertable = false;
             if (int.TryParse(input, out _))
